2ad7d8d914324dde52ff8111e69d63d2f58b479e
nouran <nouranesam189@gmail.com>
2025-08-18 18:47:58 +0300

    fix details error

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: "details/:id",
+    renderMode: RenderMode.Server
+
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
